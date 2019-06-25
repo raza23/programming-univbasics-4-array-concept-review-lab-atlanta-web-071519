ac2bb20635aa531fcadec8c9b4724389db524aaa
Raza Tahir Shareef <rtshareef@gmail.com>
2019-06-25 19:33:52 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  return array.index(value_to_find)
+
+
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  return array.sort[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  return array.sort[0]# Add your solution here
 end
